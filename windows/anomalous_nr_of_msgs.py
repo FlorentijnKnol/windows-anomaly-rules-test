d40af9e4aa5df07776c519c84b02b64028c1706b
florentijnknol <florentijn.knol@dutchsec.com>
2021-05-10 15:47:40 +0200

    new rule (unique source ips)

--- windows/anomalous_nr_of_msgs.py
+++ windows/anomalous_nr_of_msgs.py
@@ -4,11 +4,11 @@ from python_rules import Rule
 class AnomalousNrOfMessages(Rule):
     id = "c6c5990e-61df-455f-9638-34100263b028"
     title = "Anomalous nr of messages"
-    description = "Detects an anomalous amount of messages observed within the span of an hour"
+    description = "Detects an anomalous amount of messages observed within the span of six hours"
     author = "Florentijn Knol"
     date = "2021/05/10"
     level = "medium"
 
 
     def rule(self, e):
-        return self.stats.windowed("5H").is_anomaly('total_count', threshold=2.0)
+        return self.stats.windowed("6H").is_anomaly('total_count', threshold=2.0)
