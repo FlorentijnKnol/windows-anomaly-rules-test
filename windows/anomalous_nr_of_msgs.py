3bd46c2cddae1e2749269a7b775c73522c76ab86
florentijnknol <florentijn.knol@dutchsec.com>
2021-05-11 09:30:29 +0200

    test

--- windows/anomalous_nr_of_msgs.py
+++ windows/anomalous_nr_of_msgs.py
@@ -4,11 +4,11 @@ from python_rules import Rule
 class AnomalousNrOfMessages(Rule):
     id = "c6c5990e-61df-455f-9638-34100263b028"
     title = "Anomalous nr of messages"
-    description = "Detects an anomalous amount of messages observed within the span of six hours"
+    description = "Detects an anomalous amount of messages observed within the span of an hour"
     author = "Florentijn Knol"
     date = "2021/05/10"
     level = "medium"
 
 
     def rule(self, e):
-        return self.stats.windowed("10m").is_anomaly('total_count', threshold=0.3)
+        return self.stats.windowed("1H").is_anomaly('total_count', threshold=1.0)
--- windows/anomalous_nr_unique_source_ips.py
+++ windows/anomalous_nr_unique_source_ips.py
@@ -4,11 +4,11 @@ from python_rules import Rule
 class AnomalousNrOfUniqueSourceIPS(Rule):
     id = "3209dc73-7d73-4bf6-9b4b-7b50cbf59f3a"
     title = "Anomalous nr of unique source ips"
-    description = "Detects an anomalous number of unique source ips observed within the span of six hours"
+    description = "Detects an anomalous number of unique source ips observed within the span of an hour"
     author = "Florentijn Knol"
     date = "2021/05/10"
     level = "medium"
 
 
     def rule(self, e):
-        return self.stats.windowed("10m").is_anomaly(metric='n_unique', key='source.ip', threshold=1.0)
+        return self.stats.windowed("1H").is_anomaly(metric='n_unique', key='source.ip', threshold=1.0)
